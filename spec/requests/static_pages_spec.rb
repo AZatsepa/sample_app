d5cc6a18bdeb695cd1c40749c4e151eae76d4220
Artem Zatcepa <gefreiterdinets@gmail.com>
2016-08-05 19:35:59 +0300

    add provide

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,7 +44,7 @@ describe "Static pages" do
 
     it "should have the content 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("R on Rails Tutorial Sample App | About")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
 
     it "should have the content 'About Us'" do
