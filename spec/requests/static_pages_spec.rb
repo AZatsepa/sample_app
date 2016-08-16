13e0bdb4f253a78f0d381c59bf88ddb893792b50
Artem Zatcepa <gefreiterdinets@gmail.com>
2016-08-16 11:25:27 +0300

    Add users

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -18,40 +18,24 @@ describe "Static pages" do
 
     before { visit help_path }
 
-    it "should have the title 'Help'" do
-      expect(page).to have_title("#{base_title} | Help")
-    end
-
-    it "should have the content 'Help'" do
-      expect(page).to have_content('Help')
-    end
+    it { should have_title("| Help") }
+    it { should have_title(full_title('')) }
   end
 
   describe "About page" do
 
     before { visit about_path }
 
-    it "should have the content 'About'" do
-      expect(page).to have_title("#{base_title} | About")
-    end
-
-    it "should have the content 'About Us'" do
-      expect(page).to have_content('About Us')
-    end
+    it { should have_title('About')}
+    it { should have_title(full_title(''))}
   end
 
   describe 'Contact page' do
 
     before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    it { should have_title('Contact')}
+    it { should have_title(full_title(''))}
   end
 
 end
